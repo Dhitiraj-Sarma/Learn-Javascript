902881bf47140a309922ab1117528d1321ebae97
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-16 18:01:25 +0530

    commit

--- functions.js
+++ functions.js
@@ -1,17 +1,27 @@
 // Adding two numbers
 // A funciton called addNumbers takes two numbers and return the sum of the two numbers
 const addNumbers = function (a, b) {
-	return a + b;
-  };
-  
-  console.log(addNumbers(3, 5)); // 8
-  console.log(addNumbers(10, 20)); // 30
+  return a + b;
+};
 
+console.log(addNumbers(3, 5)); // 8
+console.log(addNumbers(10, 20)); // 30
 
 // creating a function of name greet and it takes a parameter name and return "Hello name"
 const greet = function (name) {
-	console.log("Hello ", name);
-  };
-  
-  greet("Dhitiraj Sarma");
-  
+  console.log("Hello ", name);
+};
+
+greet("Dhitiraj Sarma");
+
+// Creating a function that check a number is even or odd
+const isEven = function (number) {
+  if (number % 2 == 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+};
+
+console.log(isEven(4)); //  even
+console.log(isEven(5)); // odd
