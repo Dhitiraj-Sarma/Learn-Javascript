8d3ab8a3f23e300bc70eb69985760b4b87b6dcf6
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-16 17:54:23 +0530

    commit

--- functions.js
+++ functions.js
@@ -6,3 +6,12 @@ const addNumbers = function (a, b) {
   
   console.log(addNumbers(3, 5)); // 8
   console.log(addNumbers(10, 20)); // 30
+
+
+// creating a function of name greet and it takes a parameter name and return "Hello name"
+const greet = function (name) {
+	console.log("Hello ", name);
+  };
+  
+  greet("Dhitiraj Sarma");
+  
