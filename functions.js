1e481693684121d7a759f050a676854447383bfe
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-16 17:51:33 +0530

    second commit

---
+++ functions.js
@@ -0,0 +1,8 @@
+// Adding two numbers
+// A funciton called addNumbers takes two numbers and return the sum of the two numbers
+const addNumbers = function (a, b) {
+	return a + b;
+  };
+  
+  console.log(addNumbers(3, 5)); // 8
+  console.log(addNumbers(10, 20)); // 30
