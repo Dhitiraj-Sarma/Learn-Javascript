94cb503257dba94fc830a8c41ff7da205952314a
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-17 16:42:13 +0530

    commit

--- variables.js
+++ variables.js
@@ -4,3 +4,11 @@ const name = "Dhitiraj Sarma";
 const age = 21;
 
 console.log("My name is " + name + " and i am " + age + " years old.");
+
+// Calculating the Area of a cirle
+const r = 10;
+const pi = 3.14;
+
+const area = pi * r * r;
+
+console.log("The area of the circle is " + area);
