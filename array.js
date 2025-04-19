dffffd13040bd1c93ce2666486e26288697aa844
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-19 23:20:01 +0530

    commit

--- array.js
+++ array.js
@@ -3,9 +3,30 @@ const arr = [1, 2, 3, 4, 5, 6, 10]; // Give array
 
 var sum = 0; // Initialize a variable sum with value 0
 
-for (let index = 0; index < arr.length; index++) {
+for (let i = 0; i < arr.length; i++) {
   // running the for loop from 0 to 6
-  sum = sum + arr[index]; // adding each element on sum variable
+  sum = sum + arr[i]; // adding each element on sum variable
 }
 
 console.log(sum); // 31
+
+// Fidn the Maximum Numbers of an array
+
+// Findting without any built in funciton
+
+function findMax(arr) {
+  let max = arr[0]; // Initialize first element as Maximum Number
+
+  for (let i = 1; i < arr.length; i++) {
+    // Run the Loop from 1 to arr.length
+    if (arr[i] > max) {
+      max = arr[i]; // Update max if current element is greater
+    }
+  }
+
+  return max;
+}
+
+console.log(findMax([10, 12, 1, 0, -1, 30, -100])); // Output: 30
+
+
