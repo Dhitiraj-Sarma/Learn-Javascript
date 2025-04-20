0d2615830651a7e5ed2ab83c4ecffac1f2472596
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-20 23:09:51 +0530

    two sum

--- array.js
+++ array.js
@@ -95,3 +95,40 @@ function removeDuplicates(arr) {
 }
 
 console.log(removeDuplicates([1, 1, 2, 3, 2, 5, 6, 7, 5])); // Output: [ 1, 2, 3, 5, 6, 7 ]
+
+// TWO SUM
+
+function TwoSum(arr, target) {
+  let pairs = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        // Check if this pair already exists in pairs
+        let alreadyExists = false;
+
+        for (let k = 0; k < pairs.length; k++) {
+          let a = pairs[k][0];
+          let b = pairs[k][1];
+
+          // Match regardless of order
+          if (
+            (a === arr[i] && b === arr[j]) ||
+            (a === arr[j] && b === arr[i])
+          ) {
+            alreadyExists = true;
+            break;
+          }
+        }
+
+        if (!alreadyExists) {
+          pairs.push([arr[i], arr[j]]);
+        }
+      }
+    }
+  }
+
+  return pairs;
+}
+
+console.log(TwoSum([1, 2, 3, 4, 5, 6, 0, 10, -5, -1], 5)); // Output: [ [ 1, 4 ], [ 2, 3 ], [ 5, 0 ], [ 6, -1 ], [ 10, -5 ] ]
