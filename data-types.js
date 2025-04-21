74e7f5cc92cc46c010925cc9509d68d675b90878
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-21 16:42:44 +0530

    data types

---
+++ data-types.js
@@ -0,0 +1,23 @@
+/* 
+There are two types of data types
+
+1. Primitive Data Types
+	String
+
+	Number
+
+	BigInt
+
+	Boolean
+
+	Symbol
+
+	Undefined
+
+	Null
+
+2. Non‑Primitive Data Types
+
+	Object (including Array, Function, Date, etc. )
+
+*/
