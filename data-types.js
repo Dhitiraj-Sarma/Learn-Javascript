35cdb7eb79593a959c9d4c783c634207c787de4e
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-21 16:52:09 +0530

    number

--- data-types.js
+++ data-types.js
@@ -29,3 +29,13 @@ let greeting = "Hello, world!";
 let template = `Value: ${42}`;
 
 typeof greeting; // string
+
+// ----------- NUMBER DATA TYPE -----------
+// Supports special values: NaN, POSITIVE_INFINITY, NEGATIVE_INFINITY
+// Numeric limits: safe integers between -(2^53 – 1) and 2^53 – 1.
+
+let pi = 3.14;
+Number.MAX_SAFE_INTEGER; // 9007199254740991
+Number.MIN_SAFE_INTEGER; //-9007199254740991
+Number.POSITIVE_INFINITY; //Infinity
+Number.NEGATIVE_INFINITY; //-Infinity
