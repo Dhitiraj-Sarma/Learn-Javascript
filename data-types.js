a6858f5eb62f4aea84e24cab6e1c0ea0f470e70a
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-21 17:00:51 +0530

    bigint

--- data-types.js
+++ data-types.js
@@ -26,12 +26,13 @@ There are two types of data types
 //  Literals are enclosed in single, double, or back‑tick quotes
 
 let greeting = "Hello, world!";
+let name = "John";
 let template = `Value: ${42}`;
 
 typeof greeting; // string
 
 // ----------- NUMBER DATA TYPE -----------
-// Supports special values: NaN, POSITIVE_INFINITY, NEGATIVE_INFINITY
+// Supports special values like - NaN, POSITIVE_INFINITY, NEGATIVE_INFINITY
 // Numeric limits: safe integers between -(2^53 – 1) and 2^53 – 1.
 
 let pi = 3.14;
@@ -39,3 +40,11 @@ Number.MAX_SAFE_INTEGER; // 9007199254740991
 Number.MIN_SAFE_INTEGER; //-9007199254740991
 Number.POSITIVE_INFINITY; //Infinity
 Number.NEGATIVE_INFINITY; //-Infinity
+
+// -------------- BigInt ----------------
+// Arbitrary‑precision integers, denoted by an n suffix or the BigInt() constructor
+
+const big = 123456789768012345678901234567890n;
+typeof big; // bigint
+
+typeof BigInt(12341354123324255415); // bigint
