41b5b12b41726607f83d65ccd966a55da416cdb4
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-05-17 22:51:58 +0530

    operators.js

---
+++ operators.js
@@ -0,0 +1,52 @@
+// Categories of Operators
+
+// Arithmetic Operators
+// Arithmetic operators perform mathematical calculations on numeric values or variables.
+
+/*
++ (Addition)
+
+- (Subtraction)
+
+* (Multiplication)
+
+/ (Division)
+
+% (Modulus)
+
+** (Exponentiation, ES2016+)
+
+++ (Increment)
+
+-- (Decrement) 
+*/
+
+// Addition (+)
+let sum1 = 5 + 3; // 8
+let sum2 = 10 + 20; // 30
+
+// Subtraction (-)
+let diff1 = 10 - 4; // 6
+let diff2 = 7 - 15; // -8
+
+// Multiplication (*)
+let prod1 = 6 * 3; // 18
+let prod2 = 2 * 8; // 16
+
+// Division (/)
+let div1 = 12 / 4; // 3
+let div2 = 15 / 2; // 7.5
+
+// Modulus (%)
+let mod1 = 10 % 3; // 1
+let mod2 = 14 % 5; // 4
+
+// Exponentiation (**)
+let exp1 = 2 ** 3; // 8
+let exp2 = 5 ** 2; // 25
+
+// Increment (++)
+let a = 5;
+a++; // 6
+let b = 10;
+++b; // 11
