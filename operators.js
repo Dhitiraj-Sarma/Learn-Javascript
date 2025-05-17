e3e3d5b82c1d6dd2684b46006b5099dd942de773
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-05-17 22:55:02 +0530

    assignment operators

--- operators.js
+++ operators.js
@@ -50,3 +50,59 @@ let a = 5;
 a++; // 6
 let b = 10;
 ++b; // 11
+
+// Assignment Operators
+// Assignment operators assign values to variables, often by combining an operation with the simple assignment (=)
+/*
+= (Simple assignment)
+
++=, -=, *=, /=, %= (Arithmetic assignments)
+
+**= (Exponentiation assignment)
+
+<<=, >>=, >>>= (Shift assignments)
+
+&=, ^=, |= (Bitwise assignments)
+
+&&=, ||=, ??= (Logical assignments, ES2020) 
+*/
+
+// Simple assignment (=)
+let x = 10;
+let y = "hello";
+
+// Addition assignment (+=)
+let num1 = 5;
+num1 += 3; // 8
+let str1 = "Hi, ";
+str1 += "there!"; // "Hi, there!"
+
+// Subtraction assignment (-=)
+let num2 = 10;
+num2 -= 4; // 6
+let num3 = 20;
+num3 -= 15; // 5
+
+// Multiplication assignment (*=)
+let num4 = 6;
+num4 *= 3; // 18
+let num5 = 2;
+num5 *= 8; // 16
+
+// Division assignment (/=)
+let num6 = 12;
+num6 /= 4; // 3
+let num7 = 15;
+num7 /= 2; // 7.5
+
+// Modulus assignment (%=)
+let num8 = 10;
+num8 %= 3; // 1
+let num9 = 14;
+num9 %= 5; // 4
+
+// Exponentiation assignment (**=)
+let num10 = 2;
+num10 **= 3; // 8
+let num11 = 5;
+num11 **= 2; // 25
